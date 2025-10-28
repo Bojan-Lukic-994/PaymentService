d771c2dbd81187120e2e14b5d47d6f0eacd681ec
Bojan-Lukic-994 <boka.lukic.994@gmail.com>
2025-10-28 20:15:23 +0100

    Initial commit

---
+++ src/test/java/com/bojanlukic/test/PaymentTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bojanlukic.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentTransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
